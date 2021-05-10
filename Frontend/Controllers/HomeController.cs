04652b87454e34604b717e518d2d32414a2af026
rickardohcab <rickard.oh@cab.se>
2021-05-10 13:42:44 +0200

    Added backend

--- Frontend/Controllers/HomeController.cs
+++ Frontend/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace o3h.Webexample.Frontend.Controllers
@@ -23,8 +24,20 @@ namespace o3h.Webexample.Frontend.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> PrivacyAsync()
         {
+            using var httpClient = new HttpClient();
+
+            var apiClient = new swaggerClient("https://localhost:44369/", httpClient);
+            var result = await apiClient.WeatherForecastAsync();
+
+            //// create a product
+            //await apiClient.CreateProductAsync("1.1", new CreateProductRequest
+            //{
+            //    Id = 1000,
+            //    InventoryCount = 0,
+            //    Name = "Test Product"
+            //});
             return View();
         }
 
